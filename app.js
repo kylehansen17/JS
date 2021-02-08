d7886e26a59018baad51e4a50421aca60840596b
kylehansen17 <kyle.hansen925@gmail.com>
2021-02-08 07:45:13 -0600

    Update app.js

--- app.js
+++ app.js
@@ -47,9 +47,30 @@
 //     console.log("PASSWORD TOO SHORT. MUST BE 6+ CHARACTERS")
 // }
 
-const password = prompt("Enter your password");
-if (password.length >= 6 && password.indexOf(' ') === -1) {
-    console.log("VALID PASSWORD")
-} else {
-    console.log("INCORRECT FORMAT")
+// const password = prompt("Enter your password");
+// if (password.length >= 6 && password.indexOf(' ') === -1) {
+//     console.log("VALID PASSWORD")
+// } else {
+//     console.log("INCORRECT FORMAT")
+// }
+
+// 0-5 free
+// 5-10 $10
+// 10-65 $20
+// 65+ free
+
+// const age = 1;
+// if (age >= 0 && age < 5 || age >= 65) {
+//     console.log("FREE");
+// } else if (age >= 5 && age < 10) {
+//     console.log("$10")
+// } else if (age >= 10 && age < 65) {
+//     console.log("$20")
+// } else {
+//     console.log("INVALID AGE")
+// }
+
+let firstName = prompt("enter your first name");
+if (!firstName) {
+    firstName = prompt("TRY AGAIN!");
 }
